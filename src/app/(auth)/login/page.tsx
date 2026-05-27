0b691f04c0b14d7b683dc1553ea6a7ce7dae32d4
endisuwandi <endisuwandi64@gmail.com>
2026-05-27 12:33:00 +0700

    feat: add login and registration pages with form handling

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,107 @@
+"use client"; // Penanda wajib di Next.js untuk halaman yang punya interaksi
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import type { LoginResponse } from "@/modules/user/type";
+
+export default function LoginPage() {
+  const router = useRouter(); // Alat untuk pindah halaman di Next.js
+
+  // Ini adalah pengganti clientAction
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const loginBody = {
+      email: formData.get("email")?.toString(),
+      password: formData.get("password")?.toString(),
+    };
+
+    console.log(loginBody);
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(loginBody),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Gagal login, cek kredensial lu");
+      }
+
+      const loginResponse: LoginResponse = await response.json();
+
+      console.log(loginResponse);
+
+      // Asumsi token ada di dalam object response
+      //   Cookies.set("token", loginResponse.token);
+
+      // Pindah ke dashboard
+      router.push("/dashboard");
+    } catch (error) {
+      console.error(error);
+      alert("Terjadi kesalahan saat login.");
+    }
+  };
+
+  return (
+    <div className="min-h-screen grid place-items-center bg-zinc-50 p-4">
+      <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8 border border-gray-200">
+        <h1 className="text-2xl font-semibold text-center mb-6 text-gray-800">
+          Login
+        </h1>
+        <h3 className="text-sm flex justify-center space-y-2 mb-3">
+          Please enter your e-mail and password:
+        </h3>
+
+        <form onSubmit={handleLogin}>
+          <div className="space-y-2 mb-4">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              required
+              placeholder="example@mail.com"
+              className="focus-visible:ring-2 focus-visible:ring-primary"
+            />
+          </div>
+
+          <div className="space-y-2 mb-6">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              required
+              placeholder="Enter a strong password"
+              className="focus-visible:ring-2 focus-visible:ring-primary"
+            />
+          </div>
+
+          <Button type="submit" className="w-full">
+            Log In
+          </Button>
+        </form>
+
+        <p className="text-sm text-center text-gray-500 mt-4">
+          Don’t have an account?{" "}
+          <Link href="/register" className="text-primary hover:underline">
+            Register here
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import type { RegisterResponse } from "@/modules/user/type";
+
+export default function RegisterPage() {
+  const router = useRouter();
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const registerBody = {
+      username: formData.get("username")?.toString(),
+      email: formData.get("email")?.toString(),
+      password: formData.get("password")?.toString(),
+      fullName: formData.get("fullName")?.toString(),
+    };
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/register`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(registerBody),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Gagal register, periksa kembali data lu.");
+      }
+
+      const registerResponse: RegisterResponse = await response.json();
+      console.log(registerResponse);
+
+      router.push("/login");
+    } catch (error) {
+      console.error(error);
+      alert("Terjadi kesalahan saat registrasi.");
+    }
+  };
+
+  return (
+    <div className="min-h-screen grid place-items-center bg-gray-50 p-4">
+      <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8 border border-gray-200">
+        <h1 className="text-2xl font-semibold text-center mb-6 text-gray-800">
+          Register
+        </h1>
+
+        <form onSubmit={handleRegister}>
+          <div className="space-y-2 mb-4">
+            <Label htmlFor="username">Username</Label>
+            <Input
+              id="username"
+              type="text"
+              name="username"
+              required
+              placeholder="Enter your username"
+              className="focus-visible:ring-2 focus-visible:ring-primary"
+            />
+          </div>
+
+          <div className="space-y-2 mb-4">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              required
+              placeholder="example@mail.com"
+              className="focus-visible:ring-2 focus-visible:ring-primary"
+            />
+          </div>
+
+          <div className="space-y-2 mb-4">
+            <Label htmlFor="full-name">Full Name</Label>
+            <Input
+              id="full-name"
+              type="text"
+              name="fullName"
+              required
+              placeholder="Your full name"
+              className="focus-visible:ring-2 focus-visible:ring-primary"
+            />
+          </div>
+
+          <div className="space-y-2 mb-6">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              required
+              placeholder="Enter a strong password"
+              className="focus-visible:ring-2 focus-visible:ring-primary"
+            />
+          </div>
+        </form>
+
+        <p className="text-sm text-center text-gray-500 mt-4">
+          Already have an account?
+          <Link href="/login" className="text-primary hover:underline">
+            Login here
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
