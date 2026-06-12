a6a13ba6766306530e4afd8fa789566e1f35c9dc
endisuwandi <endisuwandi64@gmail.com>
2026-06-12 16:47:24 +0700

    feat: configure image optimization with remote patterns for Unsplash

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  // Lu bisa nambahin config lain di bawah sini nanti
 };
 
 export default nextConfig;
